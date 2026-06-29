7a506d1f115ade95d702e1ba511ef1002db36026
Saumya1305 <mishrasaumya134@gmail.com>
2026-06-29 20:46:58 +0530

    Initial Express project setup

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Discord Slash Command Bot is Running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
