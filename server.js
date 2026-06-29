1cc8ab34b4100dacdfd4312175ee33d275a43c6d
Saumya1305 <mishrasaumya134@gmail.com>
2026-06-30 01:27:23 +0530

    Rename interactionRoutes.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const interactionRoutes = require("./routes/interactionRoutes");
+const interactionRoutes = require("./routes/temp");
 
 const app = express();
 
